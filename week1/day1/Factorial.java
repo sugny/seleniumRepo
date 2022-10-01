0545e24628a9b2915589c1775bb928b34409a5b7
sugny <31289384+sugny@users.noreply.github.com>
2022-10-01 09:10:34 -0500

    Add files via upload

---
+++ week1/day1/Factorial.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int fact=1;
+		int input =5;
+		
+		
+		for (int i = 1; i <=input; i++) {
+			
+			
+			fact = fact*i;
+			//System.out.println(fact + " after ");
+		}
+		System.out.println("The factorial of 5 is " + fact);
+		
+	}
+
+}
+
+/*What are my learnings from this code?
+	- understood for loop iterations 
+
+*/
+
---
+++ week1/day1/FirstProgram.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class FirstProgram {
+
+	
+		
+		byte number1 = 12;
+		short number2 = 356;
+		int number3 = 25675;
+		long number4 = 12586822L; 
+		float number5 = 123.4585F;
+		double number6= 1242.41254875656458;
+		char myInitial= 'N';
+		boolean coding= true;
+		String myName = "Suganya";
+		
+		
+	}
+
+
